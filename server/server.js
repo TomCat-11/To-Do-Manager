2cd8c4ffda608c6c49d3644837607d72a3f99656
Cheluvaraj H <cheluvaraj1011@gmail.com>
2025-06-28 13:15:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,12 +17,11 @@ app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// For any route not recognized by the API, serve React
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 
 mongoose
   .connect(process.env.MONGO_URI, {
