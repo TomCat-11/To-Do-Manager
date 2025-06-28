1a3329a034cf3ac937cdbe63b3511edcb48c51e5
Cheluvaraj H <cheluvaraj1011@gmail.com>
2025-06-28 13:32:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,9 +18,11 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
+
 
 
 mongoose
